922cd3d939df1125e5ea713911fa78a35b67770d
BBrejna <bartosz.brejna@gmail.com>
2024-08-13 14:03:49 +0200

    Added routing for app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {MapComponent} from "./map/component/map/map.component";
-import {ButtonComponent} from "./tools/button/button.component";
-import {AddSpotComponent} from "./map/component/add-spot/add-spot.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MapComponent, ButtonComponent,AddSpotComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
