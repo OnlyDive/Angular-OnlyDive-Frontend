4b958be438464358fd0066488c1db747b0db3097
BBrejna <bartosz.brejna@gmail.com>
2024-08-14 10:07:45 +0200

    Moved error interpreting service to app/error

--- src/app/auth/component/sign-up/sign-up.component.ts
+++ src/app/auth/component/sign-up/sign-up.component.ts
@@ -3,7 +3,7 @@ import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 
 import { AuthService } from '../../service/auth.service';
-import { ErrorsService } from '../../service/errors.service';
+import { ErrorsService } from '../../../error/errors.service';
 import { SignUpRequest } from '../../../interface/SignUpRequest';
 import { MessageComponent } from '../../../tools/message/message.component';
 import { CommonModule } from '@angular/common';
