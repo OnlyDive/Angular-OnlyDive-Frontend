47e7993e37c77aae1f6fec78109cfcc615622e56
dominikkorwek <dominikkorwek@interia.pl>
2024-09-27 18:54:34 +0200

    add textarea Auto Resize service

--- src/app/map/component/add-comment/add-comment.component.ts
+++ src/app/map/component/add-comment/add-comment.component.ts
@@ -1,8 +1,9 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 import {SpotComment} from "../../../interface/SpotComment";
 import {AngularOnlyDiveExeption} from "../../../error/AngularOnlyDiveExeption";
+import {TextAreaAutoResizeService} from "../../../HtmlDynamicServices/text-area-auto-resize.service";
 
 @Component({
   selector: 'app-add-comment',
@@ -19,12 +20,17 @@ export class AddCommentComponent{
   @Output() onAddSpotComment = new EventEmitter<SpotComment>();
   spotCommentRequest: SpotComment = {name:"",description:""};
 
-  constructor() {}
+  constructor(private autoResize: TextAreaAutoResizeService) {}
 
   onSubmit() {
     if (this.spotCommentRequest.name ==="" || this.spotCommentRequest.description == "")
       throw new AngularOnlyDiveExeption("Please enter a valid data");
 
     this.onAddSpotComment.emit(this.spotCommentRequest);
+  }
+
+  TextAreaAutoResize(event: Event) {
+    const textarea = event.target as HTMLTextAreaElement;
+    this.autoResize.TextAreaAutoResize(textarea)
   }
 }
--- src/app/map/component/view-comments/view-comments.component.ts
+++ src/app/map/component/view-comments/view-comments.component.ts
@@ -1,10 +1,11 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild} from '@angular/core';
 import {SpotComment} from "../../../interface/SpotComment";
 import {MomentModule} from "ngx-moment";
 import {CommentService} from "../../service/comment.service";
 import {AuthService} from "../../../auth/service/auth.service";
 import {Permission} from "../../../interface/Permission";
 import {NgIf} from "@angular/common";
+import {TextAreaAutoResizeService} from "../../../HtmlDynamicServices/text-area-auto-resize.service";
 
 @Component({
   selector: 'app-view-comments',
@@ -16,13 +17,17 @@ import {NgIf} from "@angular/common";
   templateUrl: './view-comments.component.html',
   styleUrls: ['./view-comments.component.css','../../../styles/buttonStyles.css']
 })
-export class ViewCommentsComponent implements OnInit{
+export class ViewCommentsComponent implements OnInit,AfterViewInit{
   @Output() deleteEmitter = new EventEmitter<SpotComment>();
   @Input() comment!:SpotComment;
   isUserPermittedToDelete = false;
 
+  @ViewChild('autoResizeTextarea') textarea!: ElementRef;
+
+
   constructor(private commentService: CommentService,
-              private authService: AuthService) {}
+              private authService: AuthService,
+              private autoResize: TextAreaAutoResizeService) {}
 
   ngOnInit() {
     const perms:Permission = {username:this.comment.username}
@@ -34,9 +39,18 @@ export class ViewCommentsComponent implements OnInit{
     )
   }
 
+  ngAfterViewInit() {
+    this.textAreaAutoResize();
+  }
+
   deleteComment(){
     this.commentService.deleteComment(this.comment!.id!).subscribe(
       () => this.deleteEmitter.emit(this.comment)
     );
+  }
+
+  textAreaAutoResize() {
+    const textarea = this.textarea.nativeElement as HTMLTextAreaElement;
+    this.autoResize.TextAreaAutoResize(textarea)
   }
 }
