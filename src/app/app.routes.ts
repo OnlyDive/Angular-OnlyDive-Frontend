d804aef616ab2ff590015b5e7d4212b72479399d
Bartosz Brejna <bartosz.brejna@gmail.com>
2024-11-05 15:28:44 +0100

    Added userProfile component and linked it to header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { SignUpComponent } from './auth/component/sign-up/sign-up.component';
 import { LogInComponent } from './auth/component/log-in/log-in.component';
 import { VerifyAccountComponent } from './auth/component/verify-account/verify-account.component';
 import { LogOutComponent } from './auth/component/log-out/log-out.component';
+import { UserProfileComponent } from './people/user-profile/user-profile.component';
 
 export const routes: Routes = [
     { path: '', component: MapComponent },
     { path: 'signUp', component: SignUpComponent },
     { path: 'logIn', component: LogInComponent},
     { path: 'verifyAccount/:verificationToken', component: VerifyAccountComponent },
-    { path: 'logOut', component: LogOutComponent }
+    { path: 'logOut', component: LogOutComponent },
+    { path: 'profile', component: UserProfileComponent }
 ];
--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -32,6 +32,11 @@ export class HeaderComponent {
 
   }
 
+  showProfile() {
+    this.sidebarCheckbox.nativeElement.checked = false;
+    this.router.navigate(['/profile']);
+  }
+
   logOut() {
     this.sidebarCheckbox.nativeElement.checked = false;
     this.router.navigate(['/logOut']);
